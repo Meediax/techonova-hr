1652e9a588cf52e2afa1b9901507a6dc8bcf1fee
Soji Adeyanju <a.adeyanju@gmail.com>
2025-12-17 21:45:09 +0000

    Updated main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.tsx' // Import it
+import axios from 'axios';
+
+// If we are in production, use the environment variable. 
+// If in development (localhost), use the local proxy.
+// We will set VITE_API_URL in Vercel later.
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
